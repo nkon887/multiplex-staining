f1590d958b415cc04d4d2686821277b7a4d98790
Nata <nkononenko@zedat.fu-berlin.de>
2022-11-09 01:23:58 +0100

    fix: adding redundant files to .gitignore

--- py_pipeline/config.py
+++ py_pipeline/config.py
@@ -9,6 +9,6 @@ if not os.path.exists(workingDir):
 inputDir = os.path.join(workingDir, "input").replace("\\", "/")
 if not os.path.exists(inputDir):
     os.mkdir(inputDir)
-hyperstacksDir = os.path.join(inputDir, "hyperstacks").replace("\\", "/")
+hyperstacksDir = os.path.join(workingDir, "hyperstacks").replace("\\", "/")
 if not os.path.exists(hyperstacksDir):
     os.mkdir(hyperstacksDir)
