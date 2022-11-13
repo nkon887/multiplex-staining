f6db5b83fa0170796bd18e4e714eda4ec15bca3d
Nata <nkononenko@zedat.fu-berlin.de>
2022-11-14 00:35:38 +0100

    feat: adding step Four to concatenate cropped hyperstacks

---
+++ py_pipeline/stepFour_concatenate.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from ij.io import FileSaver
+from ij import IJ
+from ij.plugin import Concatenator
+sys.path.append(os.path.abspath(os.getcwd()))
+# sys.path.append(os.path.abspath("C:/Users/nko88/PycharmProjects/muliplex-staining/py_pipeline"))
+import config
+
+
+ext = ".tif"
+hsDir = config.hyperstacksDir
+outDir = config.concatenatesDir
+hsFiles = []
+concatenate_path = os.path.join(outDir, "concatenate" + ext).replace("\\", "/")
+for hs in os.listdir(hsDir):
+    if "_Cropped" in os.path.basename(hs):
+        stackToCropPath = os.path.join(hsDir, hs)
+        imp = IJ.openImage(stackToCropPath)
+        hsFiles.append(imp)
+
+concatenate = Concatenator.run(hsFiles)
+FileSaver(concatenate).saveAsTiff(concatenate_path)
