f33ef81e67174f2a36d581e98d3de0106b473568
Nata <nkononenko@zedat.fu-berlin.de>
2022-11-27 22:20:58 +0100

    fix: adding popups for exit asking and progress status, adding processing bar to see progress of copying dynamically

--- py_pipeline/copying_files.py
+++ py_pipeline/copying_files.py
@@ -1,32 +1,55 @@
+import os
+
 import PySimpleGUI as sG
-import pythontools as pt
+
 import config
+import pythontools as pt
 
 
 def main():
     font = ('Courier New', 11)
     source_dir = "-IN2-"
     submit_button = 'Submit'
-    exit_button = 'Exit'
     cancel_button = 'Cancel'
 
     sG.set_options(font=font)
     layout = [
         [sG.T("")],
-        [sG.Text("Choose a folder: "), sG.Input(key=source_dir, change_submits=True, enable_events=True),
+        [sG.Text("Choose a folder with the stiched files you want to process: "),
+         sG.Input(config.baseDir, key=source_dir, change_submits=True, enable_events=True),
          sG.FolderBrowse(key="-IN-")],
         [sG.T("")],
         [sG.Button(submit_button), sG.Button(cancel_button)]
     ]
     # Building Window
-    window = sG.Window('My File Browser', layout, keep_on_top=True, element_justification='c')
+    window = sG.Window('My File Browser', layout, keep_on_top=True, element_justification='c', finalize=True,
+                       enable_close_attempted_event=True)
     while True:
         event, values = window.read()
-        if event == sG.WIN_CLOSED or event == exit_button or event == cancel_button:
-            break
+        if event in (sG.WINDOW_CLOSE_ATTEMPTED_EVENT, sG.WIN_CLOSED, cancel_button, 'Exit', '-ESCAPE-'):
+            event, values = sG.Window('Yes/No?', [[sG.Text('Do you really want cancel/exit?')],
+                                                  [sG.Button('Yes'), sG.Button('No')]],
+                                      modal=True, element_justification='c').read(close=True)
+            if event == 'Yes':
+                break
+
         elif event == submit_button:
-            folder = values[source_dir]
-            pt.parse_dir(folder, config.inputDir)
+            src = values[source_dir]
+            des = config.inputDir
+            for dir_path, dir_names, file_names in os.walk(src):
+                for file_name in (sorted(file_names)):
+                    target_dir = dir_path.replace(src, des, 1).replace("\\", "/")
+                    if not os.path.exists(target_dir):
+                        os.mkdir(target_dir)
+                    src_file = os.path.join(dir_path, file_name).replace("\\", "/")
+                    dest_file = os.path.join(target_dir, file_name).replace("\\", "/")
+                    pt.copy_with_progress(src_file, dest_file)
+
+            event, values = sG.Window('Output', [[sG.Text('Sucessfully copied. Do you want to copy from the '
+                                                          'other source?')], [sG.Button('Yes'), sG.Button('No')]],
+                                      modal=True, element_justification='c').read(close=True)
+            if event == 'No':
+                break
 
 
 if __name__ == "__main__":
