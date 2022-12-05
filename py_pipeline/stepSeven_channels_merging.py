44cc178f5c01e1bd29084e05ceaa85aeabce3d1f
Nata <nkononenko@zedat.fu-berlin.de>
2022-12-05 02:32:45 +0100

    fix: Step Seven enable automatic processing after giving all the input parameters

--- py_pipeline/stepSeven_channels_merging.py
+++ py_pipeline/stepSeven_channels_merging.py
@@ -34,7 +34,7 @@ def getting_input_parameters(dapi_files, markers):
     gui.showDialog()
 
     if gui.wasCanceled():
-        IJ.log("User canceled dialog! Doing nothing. Exit")
+        print("User canceled dialog! Doing nothing. Exit")
         return
     params = {config.dapi_str: gui.getNextChoice()}
     for marker in markers:
@@ -46,29 +46,28 @@ def getting_input_parameters(dapi_files, markers):
 def get_channel_files(subfolder, marker):
     channel_files = []
     for subfolder_file in os.listdir(subfolder):
-        if marker in subfolder_file:
+        if marker in os.path.basename(subfolder_file):
             channel_files.append(subfolder_file)
     return channel_files
 
 
 def process(dapi_file, selected_channel_files, output_dir, force_save):
     for channel_file in selected_channel_files:
+        print(str(channel_file))
         merged_filename = (os.path.basename(channel_file)).split(".")[0] + "_merged_dapi" + config.tiff_ext
-        IJ.log(str(channel_file))
-        IJ.log(str(merged_filename))
         imp1 = IJ.openImage(dapi_file)
         imp1.show()
         imp2 = IJ.openImage(channel_file)
         imp2.show()
         IJ.run(imp1, "Merge Channels...",
-               "c1=" + os.path.basename(dapi_file) + " c3=" + os.path.basename(channel_file) + " keep")
+               "c1=" + os.path.basename(channel_file) + " c3=" + os.path.basename(dapi_file) + " keep")
         imp1.close()
         imp2.close()
         subfolder_folder_path = os.path.join(output_dir, os.path.basename(channel_file).split("_")[1])
         if not os.path.exists(subfolder_folder_path):
             os.mkdir(subfolder_folder_path)
         merged_file_path = os.path.join(subfolder_folder_path, merged_filename)
-        IJ.log(str(merged_file_path))
+        print("Saving the " + str(merged_file_path))
         if (not os.path.exists(merged_file_path)) or force_save:
             FileSaver(WindowManager.getCurrentImage()).saveAsTiff(merged_file_path)
     IJ.run("Close All")
@@ -79,30 +78,38 @@ def main():
     output_dir = config.mergeChannelsDir
     subfolders = [x[0].replace("\\", "/") for x in os.walk(input_dir)]
     subfolders.pop(0)
+    selected_markers_dict, force_save_dict = {}, {}
+    markers = []
     for subfolder in subfolders:
         dapi_files = pt.dapi_tiff_image_filenames(subfolder, config.dapi_str, config.tiff_ext)
         markers = get_channels(subfolder, config.dapi_str)
         if not dapi_files:
-            IJ.log("Image file of dapi channel isn't found. Please check the filename and change  it if needed")
+            print("Image file of dapi channel isn't found. Please check the filename and change  it if needed")
 
         if not markers:
-            IJ.log("Channels could not be identified. Please check the filenames")
+            print("Channels could not be identified. Please check the filenames")
             return
+
         try:
             selected_markers, force_save = getting_input_parameters(dapi_files, markers)
+            if selected_markers:
+                selected_markers_dict[subfolder] = selected_markers
+            if force_save:
+                force_save_dict[subfolder] = force_save
         except:
             return
+
+    for subfolder in subfolders:
         selected_channel_files = []
         for marker in markers:
-            if selected_markers.get(marker):
-                IJ.log(str(marker))
-                selected_channel_files = get_channel_files(subfolder, marker)
-        selected_dapi_image = selected_markers[config.dapi_str]
+            if selected_markers_dict[subfolder].get(marker) and marker != config.dapi_str:
+                selected_channel_files = selected_channel_files + get_channel_files(subfolder, marker)
+        selected_dapi_image = selected_markers_dict[subfolder][config.dapi_str]
         selected_dapi_image_path = os.path.join(subfolder, selected_dapi_image).replace("\\", "/")
         selected_channel_files_paths = []
         for selected_channel_file in selected_channel_files:
             selected_channel_files_paths.append(os.path.join(subfolder, selected_channel_file).replace("\\", "/"))
-        process(selected_dapi_image_path, selected_channel_files_paths, output_dir, force_save)
+        process(selected_dapi_image_path, selected_channel_files_paths, output_dir, force_save_dict[subfolder])
 
 
 if __name__ in ['__builtin__', '__main__']:
