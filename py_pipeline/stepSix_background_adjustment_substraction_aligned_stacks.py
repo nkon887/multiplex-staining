15499cfaf9b1a1f40edf6c8bf912d5e720c9370d
Nata <nkononenko@zedat.fu-berlin.de>
2022-12-05 23:28:45 +0100

    fix: Remove the contrast processing and remove the binary processing, adjusting output file names

--- py_pipeline/stepSix_background_adjustment_substraction_aligned_stacks.py
+++ py_pipeline/stepSix_background_adjustment_substraction_aligned_stacks.py
@@ -7,24 +7,10 @@ from ij.gui import GenericDialog
 from ij.io import FileSaver
 from ij.plugin.filter import BackgroundSubtracter
 from java.lang import System
-from mpicbg.ij.plugin import NormalizeLocalContrast
 
 sys.path.append(os.path.abspath(os.getcwd()))
-# sys.path.append(os.path.abspath("C:/Users/nko88/PycharmProjects/muliplex-staining/py_pipeline"))
 import config
 import jythontools as jt
-
-
-def execute_filter(ip, params):
-    """ Given an ImageProcessor ip and a dictionary of parameters, filter the ip,
-      and return the same or a new ImageProcessor of the same dimensions and type. """
-    block_radius_x = params["blockRadiusX"]
-    block_radius_y = params["blockRadiusY"]
-    stds = params["stds"]
-    center = params["center"]
-    stretch = params["stretch"]
-    NormalizeLocalContrast.run(ip, block_radius_x, block_radius_y, stds, center, stretch)
-    return ip
 
 
 def get_list_of_indices(stack):
@@ -87,43 +73,6 @@ def ask_for_bg_parameters(markers):
             "doPresmooth": gui.getNextBoolean(),
             "correctCorners": gui.getNextBoolean(),
         }
-    # print(str(params))
-    return params
-
-
-def ask_for_co_parameters(markers):
-    gui = GenericDialog("Contrast Parameter Settings")
-    for marker in markers:
-        gui.addMessage("Marker: " + marker)
-        gui.addCheckbox("General Enhance Contrast", False)
-        gui.addToSameRow()
-        gui.addCheckbox("Adjust Contrast", False)
-        gui.addToSameRow()
-        gui.addNumericField("Block Radius X", 20)
-        gui.addToSameRow()
-        gui.addNumericField("Block Radius Y", 20)
-        gui.addToSameRow()
-        gui.addNumericField("stds", 2, 0)
-        gui.addToSameRow()
-        gui.addCheckbox("center", False)
-        gui.addToSameRow()
-        gui.addCheckbox("stretch", False)
-
-    gui.showDialog()
-    if gui.wasCanceled():
-        print("User canceled dialog! Doing nothing. Exit")
-        return
-    params = {}
-    for marker in markers:
-        params[marker] = {
-            "enhanceContrast": gui.getNextBoolean(),
-            "adjustContrast": gui.getNextBoolean(),
-            "blockRadiusX": int(gui.getNextNumber()),
-            "blockRadiusY": int(gui.getNextNumber()),
-            "stds": int(gui.getNextNumber()),
-            "center": gui.getNextBoolean(),
-            "stretch": gui.getNextBoolean()
-        }
     return params
 
 
@@ -134,106 +83,63 @@ def main():
     subfolders = [x[0].replace("\\", "/") for x in os.walk(input_dir)]
     subfolders.pop(0)
     for subfolder in subfolders:
-        for tiff_file in os.listdir(subfolder):
-            if "_Cropped" in os.path.basename(tiff_file) and tiff_file.endswith(config.tiff_ext):
-                tiff_files.append(tiff_file)
-        # Subtract background
-        bs = BackgroundSubtracter()
-        markers = []
-        for tiff_file in tiff_files:
-            imp = IJ.openImage(os.path.join(subfolder, tiff_file))
-            imp.show()
-            imp.changes = False
-            stack = imp.getStack()
-            _, markerslice_groups = get_list_of_indices(stack)
-            imp.close()
-            markers = markers + list(set(markerslice_groups.keys()))
-        markers = list(set(markers))
-        try:
-            params_bg = ask_for_bg_parameters(markers)
-            params_co = ask_for_co_parameters(markers)
-            force_save = jt.ask_to_overwrite()
-            # print(str(ask_for_bg_parameters(markers)))
-        except:
-            print("user canceled dialog. Exit")
-            return
+        all_files = os.listdir(subfolder)
+        if all_files:
+            for tiff_file in os.listdir(subfolder):
+                if "_Cropped" in os.path.basename(tiff_file) and tiff_file.endswith(config.tiff_ext):
+                    tiff_files.append(tiff_file)
+            # Subtract background
+            bs = BackgroundSubtracter()
+            markers = []
+            for tiff_file in tiff_files:
+                imp = IJ.openImage(os.path.join(subfolder, tiff_file))
+                imp.show()
+                imp.changes = False
+                stack = imp.getStack()
+                _, markerslice_groups = get_list_of_indices(stack)
+                imp.close()
+                markers = markers + list(set(markerslice_groups.keys()))
+            markers = list(set(markers))
+            try:
+                params_bg = ask_for_bg_parameters(markers)
+                force_save = jt.ask_to_overwrite()
+            except:
+                print("user canceled dialog. Exit")
+                return
 
-        for tiff_file in tiff_files:
-            print("Processing the file " + str(tiff_file))
-            imp = IJ.openImage(os.path.join(subfolder, tiff_file))
-            imp.show()
-            imp.changes = False
-            stack = imp.getStack()
-            filenames, markerslice_groups = get_list_of_indices(stack)
-            for sliceIndex in range(1, stack.getSize() + 1):
-                filename = str(stack.getSliceLabel(sliceIndex)).split(".")[0]
-                print("Processing slice " + str(sliceIndex) + " " + str(stack.getSliceLabel(sliceIndex)))
-                # Save output
-                ip = stack.getProcessor(sliceIndex)
-                slice_file_name_one = ''
-                slice_file_name_two = ''
-                slice_file_name_three = ''
-                slice_file_name_four = ''
-                marker = ''
-                subfolder_name = os.path.basename(tiff_file).split('.')[0].split("_")[0]
-                subfolder_path = os.path.join(output_dir, subfolder_name).replace("\\", "/")
-                if not os.path.exists(subfolder_path):
-                    os.mkdir(subfolder_path)
-                for marker in markerslice_groups.keys():
-                    if sliceIndex in markerslice_groups.get(marker):
-                        print("Slice " + str(sliceIndex) + " is in " + str(marker))
-                        slice_file_name_one = os.path.join(subfolder_path,
-                                                           filename + "_auto_contrast_no_background_sub"
-                                                           + ".tif").replace("\\", "/")
-                        slice_file_name_two = os.path.join(subfolder_path,
-                                                           filename + "_auto_contrast_background_sub_"
-                                                           + ".tif").replace("\\", "/")
-                        slice_file_name_three = os.path.join(subfolder_path,
-                                                             filename + "_no_contrast_no_background_sub_"
-                                                             + ".tif").replace("\\", "/")
-                        slice_file_name_four = os.path.join(subfolder_path,
-                                                            filename + "_no_contrast_background_sub_"
-                                                            + ".tif").replace("\\", "/")
-                        # Save output
-                        if params_co[marker]["adjustContrast"]:
-                            if (not os.path.exists(slice_file_name_one)) or force_save:
-
-                                print("Contrast Adjustment")
-                                try:
-                                    ip = execute_filter(ip, params_co[marker])
-                                # Fail-safe execution of the filter, which is a global function name
-                                except:
-                                    print(sys.exc_info())
-                                temp = ImagePlus(str(sliceIndex), ip)
-                                if params_co[marker]["enhanceContrast"]:
-                                    IJ.run(temp, "Enhance Contrast...", "saturated=0.35 normalize")
-                                IJ.run(temp, "8-bit", "")
-                                IJ.run(temp, "Make Binary", "method=Default background=Default calculate black")
-                                FileSaver(temp).saveAsTiff(slice_file_name_one)
-                                if (not os.path.exists(slice_file_name_two)) or force_save:
-                                    bs.rollingBallBackground(ip, params_bg[marker]["radius"],
-                                                             params_bg[marker]["createBackground"],
-                                                             params_bg[marker]["lightBackground"],
-                                                             params_bg[marker]["useParaboloid"],
-                                                             params_bg[marker]["doPresmooth"],
-                                                             params_bg[marker]["correctCorners"])
-                                    temp = ImagePlus(str(sliceIndex), ip)
-                                    if params_co[marker]["enhanceContrast"]:
-                                        IJ.run(temp, "Enhance Contrast...", "saturated=0.35 normalize")
-                                    IJ.run(temp, "8-bit", "")
-                                    IJ.run(temp, "Make Binary", "method=Default background=Default calculate black")
-                                    FileSaver(temp).saveAsTiff(slice_file_name_two)
-                                else:
-                                    print(slice_file_name_two + " exists. Doing nothing. Skipping")
-                            else:
-                                print(slice_file_name_one + " exists. Doing nothing. Skipping")
-                        else:
+            for tiff_file in tiff_files:
+                print("Processing the file " + str(tiff_file))
+                imp = IJ.openImage(os.path.join(subfolder, tiff_file))
+                imp.show()
+                imp.changes = False
+                stack = imp.getStack()
+                filenames, markerslice_groups = get_list_of_indices(stack)
+                for sliceIndex in range(1, stack.getSize() + 1):
+                    filename = str(stack.getSliceLabel(sliceIndex)).split(".")[0]
+                    print("Saving the slice " + str(sliceIndex) + " " + str(stack.getSliceLabel(sliceIndex)))
+                    # Save output
+                    ip = stack.getProcessor(sliceIndex)
+                    slice_file_name_three = ''
+                    slice_file_name_four = ''
+                    marker = ''
+                    subfolder_name = os.path.basename(tiff_file).split('.')[0].split("_")[0]
+                    subfolder_path = os.path.join(output_dir, subfolder_name).replace("\\", "/")
+                    if not os.path.exists(subfolder_path):
+                        os.mkdir(subfolder_path)
+                    for marker in markerslice_groups.keys():
+                        if sliceIndex in markerslice_groups.get(marker):
+                            print("Slice " + str(sliceIndex) + " is in " + str(marker))
+                            slice_file_name_three = os.path.join(subfolder_path,
+                                                                 filename + "no_background_sub"
+                                                                 + ".tif").replace("\\", "/")
+                            slice_file_name_four = os.path.join(subfolder_path,
+                                                                filename + "_background_sub"
+                                                                + ".tif").replace("\\", "/")
+                            # Save output
                             if (not os.path.exists(slice_file_name_three)) or force_save:
                                 temp = ImagePlus(str(sliceIndex), ip)
-                                if params_co[marker]["enhanceContrast"]:
-                                    IJ.run(temp, "Enhance Contrast...", "saturated=0.35 normalize")
+                                IJ.run(temp, "Enhance Contrast...", "saturated=0.35 normalize")
                                 IJ.run(temp, "8-bit", "")
-                                IJ.run(temp, "Make Binary", "method=Default background=Default calculate black")
                                 FileSaver(temp).saveAsTiff(slice_file_name_three)
                             else:
                                 print(slice_file_name_three + " exists. Doing nothing. Skipping")
@@ -245,17 +151,15 @@ def main():
                                                          params_bg[marker]["doPresmooth"],
                                                          params_bg[marker]["correctCorners"])
                                 temp = ImagePlus(str(sliceIndex), ip)
-                                if params_co[marker]["enhanceContrast"]:
-                                    IJ.run(temp, "Enhance Contrast...", "saturated=0.35 normalize")
+                                IJ.run(temp, "Enhance Contrast...", "saturated=0.35 normalize")
                                 IJ.run(temp, "8-bit", "")
-                                IJ.run(temp, "Make Binary", "method=Default background=Default calculate black")
                                 FileSaver(temp).saveAsTiff(slice_file_name_four)
                             else:
                                 print(slice_file_name_four + " exists. Doing nothing. Skipping")
 
-            imp.close()
-        print("Run is finished")
-        IJ.run("Close All")
+                imp.close()
+            print("Run is finished")
+            IJ.run("Close All")
 
 
 if __name__ in ['__builtin__', '__main__']:
