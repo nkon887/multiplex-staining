4ef5a813697ae7ae32f41d18ef0fef723873443d
Nata <nkononenko@zedat.fu-berlin.de>
2022-12-06 07:10:51 +0100

    fix: stepThree small correction of output paths

--- py_pipeline/stepThree_cropping_tiff_images.py
+++ py_pipeline/stepThree_cropping_tiff_images.py
@@ -54,11 +54,13 @@ def main():
                 tiff_files.append(tiff_file)
         first_roi = []
         imps = []
+        tiff_cropped_paths = []
         for tiff_file in tiff_files:
             print("Processing the tiff file " + tiff_file)
             tiff_cropped_path = os.path.join(subfolder,
                                              os.path.basename(tiff_file).split('.')[0] + config.cropped_suffix +
                                              config.tiff_ext).replace("\\", "/")
+            tiff_cropped_paths.append(tiff_cropped_path)
             # Save output
             if (not os.path.exists(tiff_cropped_path)) or force_save:
                 path = os.path.join(subfolder, tiff_file).replace("\\", "/")
@@ -77,38 +79,38 @@ def main():
                 print("The cropped tiff file " + tiff_cropped_path + " exists. Skipping")
         if imps:
             for imp in imps:
-                    imp.show()
-                    IJ.run(imp, "Enhance Contrast", "saturated=0.35")
-                    # ask the user to define a selection and get the bounds of the selection
-                    if not first_roi:
-                        IJ.setTool(Toolbar.RECTANGLE)
-                        WaitForUserDialog("Select the area using \"Rectangle\" as a form,then click OK.").show()
-                        roi = imp.getRoi()
-                        first_roi.append(roi)
-                    else:
-                        imp.setRoi(first_roi[0])
-            WaitForUserDialog("Adjust the area,then click OK.").show()
-            for imp in imps:
+                imp.show()
+                IJ.run(imp, "Enhance Contrast", "saturated=0.35")
+                # ask the user to define a selection and get the bounds of the selection
+                if not first_roi:
+                    IJ.setTool(Toolbar.RECTANGLE)
+                    WaitForUserDialog("Select the area using \"Rectangle\" as a form,then click OK.").show()
                     roi = imp.getRoi()
-                    imp.setRoi(roi)
-                    roi_width = int(first_roi[0].getFloatWidth())
-                    roi_height = int(first_roi[0].getFloatHeight())
-                    stack = imp.getStack()
-                    cropped_stack = CroppedStack(stack, roi)
-                    res_stack = ImageStack(roi_width, roi_height)
-                    IJ.resetMinAndMax(imp)
-                    for i in range(1, cropped_stack.size() + 1):
-                        res_stack.addSlice(stack.getSliceLabel(i), cropped_stack.getProcessor(i))
-                    cropped = ImagePlus("cropped", res_stack)
-                    # alternative:
-                    # cropped = imp.resize(int(roi_width), int(roi_height), 1, "bilinear")
-                    print("Saving the cropped hyperstack as " + tiff_cropped_path)
-                    if param == "hyperstack":
-                        FileSaver(HyperStackConverter.toHyperStack(cropped, cropped.getNSlices(), 1, 1, "xyczt(default)",
-                                                                   "Grayscale")).saveAsTiff(tiff_cropped_path)
-                    elif param == "alignedStack":
-                        FileSaver(cropped).saveAsTiff(tiff_cropped_path)
-                    imp.close()
+                    first_roi.append(roi)
+                else:
+                    imp.setRoi(first_roi[0])
+            WaitForUserDialog("Adjust the area,then click OK.").show()
+            for imp, tiff_cropped_path in zip(imps, tiff_cropped_paths):
+                roi = imp.getRoi()
+                imp.setRoi(roi)
+                roi_width = int(first_roi[0].getFloatWidth())
+                roi_height = int(first_roi[0].getFloatHeight())
+                stack = imp.getStack()
+                cropped_stack = CroppedStack(stack, roi)
+                res_stack = ImageStack(roi_width, roi_height)
+                IJ.resetMinAndMax(imp)
+                for i in range(1, cropped_stack.size() + 1):
+                    res_stack.addSlice(stack.getSliceLabel(i), cropped_stack.getProcessor(i))
+                cropped = ImagePlus("cropped", res_stack)
+                # alternative:
+                # cropped = imp.resize(int(roi_width), int(roi_height), 1, "bilinear")
+                print("Saving the cropped hyperstack as " + tiff_cropped_path)
+                if param == "hyperstack":
+                    FileSaver(HyperStackConverter.toHyperStack(cropped, cropped.getNSlices(), 1, 1, "xyczt(default)",
+                                                               "Grayscale")).saveAsTiff(tiff_cropped_path)
+                elif param == "alignedStack":
+                    FileSaver(cropped).saveAsTiff(tiff_cropped_path)
+                imp.close()
     print("Run is finished")
 
 
