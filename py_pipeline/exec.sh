e0be5c8027a6414b767cf34c1c718af4014621e1
nkon887 <66092531+nkon887@users.noreply.github.com>
2023-01-16 17:43:22 +0100

    fix: stepSeven_channels_merging.py to stepEight_channels_merging.py change name of the script adjust exec file accordingly

--- py_pipeline/exec.sh
+++ py_pipeline/exec.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env sh
 clear
-echo "RUNNING STEP 1 IMAGE PREPARATION"
-python stepOne_imagePreparation.py
-echo  "RUNNING STEP 2 GENERATE HYPERSTACKS"
-C:/Program\ Files\ \(x86\)/fiji-win64/Fiji.app/ImageJ-win64.exe --ij2 --run stepTwo_generate_hyperstacks.py
-echo  "RUNNING STEP 3 CROPPING AND CONCATENATE TIFF IMAGES"
-C:/Program\ Files\ \(x86\)/fiji-win64/Fiji.app/ImageJ-win64.exe --ij2 --run stepThree_cropping_tiff_images.py
-echo "RUNNING STEP 5 ALIGN CONCATENATES"
-C:/Program\ Files\ \(x86\)/fiji-win64/Fiji.app/ImageJ-win64.exe --ij2 --run stepFive_align_concatenates.py
-echo  "RUNNING STEP 6 CROPPING ALIGNED HYPERSTACKS"
-C:/Program\ Files\ \(x86\)/fiji-win64/Fiji.app/ImageJ-win64.exe --ij2 --run stepSix_cropping_aligned_hyperstacks.py
-echo "RUNNING STEP 7 BACKGROUND ADJUSTMENT SUBSTRACTION ALIGNED STACKS"
-C:/Program\ Files\ \(x86\)/fiji-win64/Fiji.app/ImageJ-win64.exe --ij2 --run stepSeven_background_adjustment_substraction_aligned_stacks.py
-echo "RUNNING STEP 8 MERGING OF CHANNELS"
-C:/Program\ Files\ \(x86\)/fiji-win64/Fiji.app/ImageJ-win64.exe --ij2 --run stepEight_channels_merging.py
+echo "MULTIPLEX IMAGE PREPROCESSING"
+#STITCHING
+#echo "RUNNING STEP 1 IMAGE PREPARATION"
+#python stepOne_imagePreparation.py
+#echo  "RUNNING STEP 2 ALIGNMENT"
+#"C:\Users\nko88\Desktop\Fiji.app\ImageJ-win64.exe" --ij2 --run stepTwo_sv_alignment.py
+echo  "RUNNING STEP 3 GENERATE HYPERSTACKS"
+"C:\Users\nko88\Desktop\Fiji.app\ImageJ-win64.exe" --ij2 --run stepThree_generate_hyperstacks.py
+#echo  "RUNNING STEP 4 CROPPING TIFF IMAGES"
+#"C:\Users\nko88\Desktop\Fiji.app\ImageJ-win64.exe" --ij2 --run stepFour_cropping.py
+#echo  "RUNNING STEP 5 ALIGNMENT PRECROP"
+#"C:\Users\nko88\Desktop\Fiji.app\ImageJ-win64.exe" --ij2 --run stepTwo_sv_alignment.py
+#echo  "RUNNING STEP 6 CROPPING ALIGNED HYPERSTACKS"
+#C:/Program\ Files\ \(x86\)/fiji-win64/Fiji.app/ImageJ-win64.exe --ij2 --run stepFive_cropping_aligned_hyperstacks.py
+#echo "RUNNING STEP 7 BACKGROUND ADJUSTMENT SUBSTRACTION ALIGNED STACKS"
+#C:/Program\ Files\ \(x86\)/fiji-win64/Fiji.app/ImageJ-win64.exe --ij2 --run stepSeven_background_adjustment_substraction_aligned_stacks.py
+#echo "RUNNING STEP 8 MERGING OF CHANNELS"
+#C:/Program\ Files\ \(x86\)/fiji-win64/Fiji.app/ImageJ-win64.exe --ij2 --run stepEight_channels_merging.py
+#contrast
+#SEGMENTATION
+#binary mask
+#CELLPROFILER ANALYSIS
+#R STATISTICS
 exit 0
