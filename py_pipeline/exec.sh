a23cb8d70b2ab83afbe8f2ef9ebe0021456c7c54
nkon887 <66092531+nkon887@users.noreply.github.com>
2023-01-22 15:11:04 +0100

    fix: adjust Path of fiji folder --> system variable

--- py_pipeline/exec.sh
+++ py_pipeline/exec.sh
@@ -3,21 +3,21 @@ clear
 echo "MULTIPLEX IMAGE PREPROCESSING"
 #STITCHING
 echo "RUNNING STEP 1 IMAGE PREPARATION"
-python ImagePreparation.py
+python image_preparation.py
 echo  "RUNNING STEP 2 ALIGNMENT"
-"C:\Users\nko88\Desktop\Fiji.app\ImageJ-win64.exe" --ij2 --run Alignment.py
-echo  "RUNNING STEP 3 GENERATE HYPERSTACKS"
-"C:\Users\nko88\Desktop\Fiji.app\ImageJ-win64.exe" --ij2 --run HyperstackGeneration.py
+$FIJIPATH --ij2 --run alignment.py
+echo  "RUNNING STEP 3 HYPERSTACK GENERATION OF NOT ALIGNED DATA"
+$FIJIPATH --ij2 --run hyperstack_generation.py
 echo  "RUNNING STEP 4 CROPPING TIFF IMAGES"
-"C:\Users\nko88\Desktop\Fiji.app\ImageJ-win64.exe" --ij2 --run CroppingForAlignment.py
+$FIJIPATH --ij2 --run cropping_for_alignment.py
 echo  "RUNNING STEP 5 ALIGNMENT PRECROP"
-"C:\Users\nko88\Desktop\Fiji.app\ImageJ-win64.exe" --ij2 --run Alignment.py
+$FIJIPATH --ij2 --run alignment.py
 echo  "RUNNING STEP 6 CROPPING ALIGNED HYPERSTACKS"
-C:/Program\ Files\ \(x86\)/fiji-win64/Fiji.app/ImageJ-win64.exe --ij2 --run CroppingAlignedHyperstacks.py
+$FIJIPATH --ij2 --run cropping_aligned_hyperstacks.py
 echo "RUNNING STEP 7 BACKGROUND ADJUSTMENT SUBSTRACTION ALIGNED STACKS"
-C:/Program\ Files\ \(x86\)/fiji-win64/Fiji.app/ImageJ-win64.exe --ij2 --run BackgroundProcessing.py
+$FIJIPATH --ij2 --run background_processing.py
 echo "RUNNING STEP 8 MERGING OF CHANNELS"
-C:/Program\ Files\ \(x86\)/fiji-win64/Fiji.app/ImageJ-win64.exe --ij2 --run MergingChannels.py
+$FIJIPATH --ij2 --run merging_channels.py
 #contrast
 #SEGMENTATION
 #binary mask
