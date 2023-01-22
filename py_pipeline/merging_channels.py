996c424078c4cbe6b6ad49c795b1228af7f2921e
nkon887 <66092531+nkon887@users.noreply.github.com>
2023-01-22 12:35:11 +0100

    fix: merging step refactor

--- py_pipeline/merging_channels.py
+++ py_pipeline/merging_channels.py
@@ -13,11 +13,17 @@ import pythontools as pt
 
 
 class MergingChannels:
+    def __init__(self, input_dir, output_dir, tiff_ext, dapi_str):
+        self.input_dir = input_dir
+        self.output_dir = output_dir
+        self.tiff_ext = tiff_ext
+        self.dapi_str = dapi_str
+
     def get_channels(self, subfolder, exc_channel):
         channels = []
         for subfolder_file in os.listdir(subfolder):
             filename = os.path.basename(subfolder_file)
-            if filename.endswith(config.tiff_ext) and not (exc_channel in filename):
+            if filename.endswith(self.tiff_ext) and not (exc_channel in filename):
                 channel = '_'.join(filename.split('.')[0].split('_')[2:])
                 channels.append(channel)
         return sorted(list(set(channels)))
@@ -44,7 +50,7 @@ class MergingChannels:
             return
         params = {}
         for subfolder in dapi_files.keys():
-            params[config.dapi_str + subfolder] = gui.getNextChoice()
+            params[self.dapi_str + subfolder] = gui.getNextChoice()
         for marker in markers:
             params[marker] = gui.getNextBoolean()
         force_save = gui.getNextBoolean()
@@ -54,15 +60,15 @@ class MergingChannels:
         channel_files = []
         for subfolder_file in os.listdir(subfolder):
             pattern = r'^\d{6}[^{\.}]*'
-            if marker in os.path.basename(subfolder_file) and re.match(pattern + r'.tif',
+            if marker in os.path.basename(subfolder_file) and re.match(pattern + self.tiff_ext,
                                                                        os.path.basename(subfolder_file)):
                 channel_files.append(subfolder_file)
         return channel_files
 
-    def process(self, dapi_file, selected_channel_files, output_dir, force_save):
+    def merging(self, dapi_file, selected_channel_files, output_dir, force_save):
         for channel_file in selected_channel_files:
             print(str(channel_file))
-            merged_filename = (os.path.basename(channel_file)).split(".")[0] + "_merged_dapi" + config.tiff_ext
+            merged_filename = (os.path.basename(channel_file)).split(".")[0] + "_merged_dapi" + self.tiff_ext
             imp1 = IJ.openImage(dapi_file)
             imp1.show()
             imp2 = IJ.openImage(channel_file)
@@ -82,47 +88,50 @@ class MergingChannels:
                 res.close()
         IJ.run("Close All")
 
+    def processing(self):
+        input_dir = self.input_dir
+        output_dir = self.output_dir
+        subfolders = [x[0].replace("\\", "/") for x in os.walk(input_dir)]
+        subfolders.pop(0)
+        if not subfolders:
+            print(input_dir + " is empty. Doing nothing")
+            return
+        markers = []
+        dapi_files_dict = {}
+
+        for subfolder in subfolders:
+            dapi_files = pt.dapi_tiff_image_filenames(subfolder, self.dapi_str, self.tiff_ext)
+            dapi_files_dict[subfolder] = dapi_files
+            markers = markers + self.get_channels(subfolder, self.dapi_str)
+            if not dapi_files:
+                print("Image file of dapi channel isn't found. Please check the filename and change  it if needed")
+            if not markers:
+                print("Channels could not be identified. Please check the filenames")
+                return
+        markers = list(set(markers))
+        try:
+            selected_markers, force_save = self.getting_input_parameters(dapi_files_dict, markers)
+        except:
+            print("Could get not the input parameters. Exiting")
+            return
+
+        for subfolder in subfolders:
+            selected_channel_files = []
+            for marker in markers:
+                if selected_markers.get(marker) and marker != self.dapi_str + subfolder:
+                    selected_channel_files = selected_channel_files + self.get_channel_files(subfolder,
+                                                                                                         marker)
+            selected_dapi_image = selected_markers[self.dapi_str + subfolder]
+            selected_dapi_image_path = os.path.join(subfolder, selected_dapi_image).replace("\\", "/")
+            selected_channel_files_paths = []
+            for selected_channel_file in selected_channel_files:
+                selected_channel_files_paths.append(
+                    os.path.join(subfolder, selected_channel_file).replace("\\", "/"))
+            self.merging(selected_dapi_image_path, selected_channel_files_paths, output_dir, force_save)
+
 
 def main():
-    input_dir = config.contrastBgAdjustDir
-    output_dir = config.mergeChannelsDir
-    subfolders = [x[0].replace("\\", "/") for x in os.walk(input_dir)]
-    subfolders.pop(0)
-    if not subfolders:
-        print(input_dir + " is empty. Doing nothing")
-        return
-    selected_markers_dict, force_save_dict = {}, {}
-    markers = []
-    dapi_files_dict = {}
-    merging_channels = MergingChannels()
-
-    for subfolder in subfolders:
-        dapi_files = pt.dapi_tiff_image_filenames(subfolder, config.dapi_str, config.tiff_ext)
-        dapi_files_dict[subfolder] = dapi_files
-        markers = markers + merging_channels.get_channels(subfolder, config.dapi_str)
-        if not dapi_files:
-            print("Image file of dapi channel isn't found. Please check the filename and change  it if needed")
-        if not markers:
-            print("Channels could not be identified. Please check the filenames")
-            return
-    markers = list(set(markers))
-    try:
-        selected_markers, force_save = merging_channels.getting_input_parameters(dapi_files_dict, markers)
-    except:
-        print("Could get not the input parameters. Exiting")
-        return
-
-    for subfolder in subfolders:
-        selected_channel_files = []
-        for marker in markers:
-            if selected_markers.get(marker) and marker != config.dapi_str + subfolder:
-                selected_channel_files = selected_channel_files + merging_channels.get_channel_files(subfolder, marker)
-        selected_dapi_image = selected_markers[config.dapi_str + subfolder]
-        selected_dapi_image_path = os.path.join(subfolder, selected_dapi_image).replace("\\", "/")
-        selected_channel_files_paths = []
-        for selected_channel_file in selected_channel_files:
-            selected_channel_files_paths.append(os.path.join(subfolder, selected_channel_file).replace("\\", "/"))
-        merging_channels.process(selected_dapi_image_path, selected_channel_files_paths, output_dir, force_save)
+    MergingChannels(config.bg_adjust_dir, config.merge_channels_dir, config.tiff_ext, config.dapi_str).processing()
 
 
 if __name__ in ['__builtin__', '__main__']:
