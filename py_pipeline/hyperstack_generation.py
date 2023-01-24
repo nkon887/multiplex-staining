5383b4f2ceac901fd8e7a6179dc0555c73f1a15f
nkon887 <66092531+nkon887@users.noreply.github.com>
2023-01-24 05:38:24 +0100

    fix:remove cropping alignment hyperstacks and cropping for alignment as they are redundant. they are integrated main.py merging channels remove main function as it is used in main.py created main.py is main execution file hyperstack_generation.py remove main function as it used in main.py

--- py_pipeline/hyperstack_generation.py
+++ py_pipeline/hyperstack_generation.py
@@ -7,14 +7,12 @@
 import os
 import shutil
 import sys
-import time
 import re
 
 from ij import IJ, ImagePlus, VirtualStack
 from ij.gui import GenericDialog
 from ij.io import FileSaver
 from ij.plugin import HyperStackConverter
-from java.lang import System
 
 sys.path.append(os.path.abspath(os.getcwd()))
 import config
@@ -23,8 +21,10 @@ import jythontools as jt
 
 
 class HyperstackGeneration:
-    def __init__(self, input_dir):
+    def __init__(self, input_dir, stacks_dir, tiff_ext):
         self.input_dir = input_dir
+        self.stacks_dir = stacks_dir
+        self.tiff_ext = tiff_ext
 
     def ask_for_parameters(self):
         gui = GenericDialog("Input parameters")
@@ -70,6 +70,68 @@ class HyperstackGeneration:
             if not os.path.exists(file_destination):
                 shutil.copy(filename, file_destination)
 
+    def generate_hyperstack(self):
+        try:
+            # Input Parameters
+            update_input_dir, params_hyperstack, force_save = self.ask_for_parameters()
+        except:
+            # user canceled dialog
+            return
+        if not os.path.exists(update_input_dir):
+            print("The input directory doesn't exist. Doing nothing.Exiting")
+            return
+        pattern = r'^\d{6}\_[^\_]*'
+        subdirs = [x[0] for x in os.walk(update_input_dir) if re.match(pattern, os.path.basename(x[0]))]
+        if not subdirs:
+            print(update_input_dir + " is empty. Doing nothing")
+            return
+
+        for subdir in subdirs:
+            hyperstack_name = os.path.basename(subdir)
+            vs = None
+            width, height = 0, 0
+            dirpath = os.path.join(update_input_dir, subdir)
+            dapifiles = pt.dapi_tiff_image_filenames(dirpath, config.dapi_str, self.tiff_ext)
+            if not dapifiles == []:
+                dapipath = os.path.join(dirpath, dapifiles[0])
+                print("Processing the subfolder " + os.path.dirname(dapipath))
+                try:
+                    width, height = jt.dimensions_of(dapipath, self.stacks_dir, config.error_subfolder_name)
+                except TypeError:
+                    print(sys.exc_info())
+                # Upon finding the dapi image, initialize the VirtualStack
+                if vs is None and self.get_files_number(dirpath, self.tiff_ext) > 1:
+                    vs = CreateVirtualStack(width, height, dirpath)
+                    hyperstack_folder = hyperstack_name.split("_")[1].split(".")[0]
+                    hyperstack_folder_path = os.path.join(self.stacks_dir, hyperstack_folder)
+                    if not os.path.exists(hyperstack_folder_path):
+                        os.mkdir(hyperstack_folder_path)
+                    hyperstack_path = os.path.join(hyperstack_folder_path, hyperstack_name + self.tiff_ext).replace(
+                        "\\",
+                        "/")
+                    # Save output
+                    if (not os.path.exists(hyperstack_path)) or force_save:
+                        print("Saving the hyperstack as " + hyperstack_path)
+                        stack = ImagePlus(self.stack_name, vs)
+                        number_channels = stack.getNSlices()
+                        FileSaver(
+                            HyperStackConverter.toHyperStack(stack, number_channels, params_hyperstack["number_frames"],
+                                                             params_hyperstack["number_slices"],
+                                                             params_hyperstack["order"],
+                                                             params_hyperstack["color"])).saveAsTiff(hyperstack_path)
+                    else:
+                        print("The hyperstack file " + hyperstack_path + " exists. Skipping")
+                elif vs is None and self.get_files_number(dirpath, self.tiff_ext) == 1:
+                    print("The number of image files is less than 2. For hyperstack it should be at least 2. Skipping")
+                    continue
+            if os.path.abspath(update_input_dir) == os.path.abspath(self.input_dir):
+                for filename in os.listdir(subdir):
+                    os.remove(os.path.join(subdir, filename))
+                shutil.rmtree(os.path.join(subdir))
+
+        print("Run is finished")
+        return
+
 
 class CreateVirtualStack(VirtualStack):
     def __init__(self, width, height, source_dir):
@@ -100,78 +162,3 @@ class CreateVirtualStack(VirtualStack):
         # Subtract background
         ip = imp.getProcessor()
         return ip
-
-
-def main():
-    precrop_input_dir = config.precrop_input_dir
-    stacks_dir = config.stacks_dir
-    tiff_ext = config.tiff_ext
-    hyperstack_generate = HyperstackGeneration(precrop_input_dir)
-    try:
-        # Input Parameters
-        update_input_dir, params_hyperstack, force_save = hyperstack_generate.ask_for_parameters()
-    except:
-        # user canceled dialog
-        return
-    if not os.path.exists(update_input_dir):
-        print("The input directory doesn't exist. Doing nothing.Exiting")
-        return
-    pattern = r'^\d{6}\_[^\_]*'
-    subdirs = [x[0] for x in os.walk(update_input_dir) if re.match(pattern, os.path.basename(x[0]))]
-    if not subdirs:
-        print(update_input_dir + " is empty. Doing nothing")
-        return
-
-    for subdir in subdirs:
-        hyperstack_name = os.path.basename(subdir)
-        vs = None
-        width, height = 0, 0
-        dirpath = os.path.join(update_input_dir, subdir)
-        dapifiles = pt.dapi_tiff_image_filenames(dirpath, config.dapi_str, tiff_ext)
-        if not dapifiles == []:
-            dapipath = os.path.join(dirpath, dapifiles[0])
-            print("Processing the subfolder " + os.path.dirname(dapipath))
-            try:
-                width, height = jt.dimensions_of(dapipath, stacks_dir, config.error_subfolder_name)
-            except TypeError:
-                print(sys.exc_info())
-            # Upon finding the dapi image, initialize the VirtualStack
-            if vs is None and hyperstack_generate.get_files_number(dirpath, tiff_ext) > 1:
-                vs = CreateVirtualStack(width, height, dirpath)
-                hyperstack_folder = hyperstack_name.split("_")[1].split(".")[0]
-                hyperstack_folder_path = os.path.join(stacks_dir, hyperstack_folder)
-                if not os.path.exists(hyperstack_folder_path):
-                    os.mkdir(hyperstack_folder_path)
-                hyperstack_path = os.path.join(hyperstack_folder_path, hyperstack_name + tiff_ext).replace("\\",
-                                                                                                           "/")
-                # Save output
-                if (not os.path.exists(hyperstack_path)) or force_save:
-                    print("Saving the hyperstack as " + hyperstack_path)
-                    stack = ImagePlus(config.stack_name, vs)
-                    number_channels = stack.getNSlices()
-                    FileSaver(
-                        HyperStackConverter.toHyperStack(stack, number_channels, params_hyperstack["number_frames"],
-                                                         params_hyperstack["number_slices"],
-                                                         params_hyperstack["order"],
-                                                         params_hyperstack["color"])).saveAsTiff(hyperstack_path)
-                else:
-                    print("The hyperstack file " + hyperstack_path + " exists. Skipping")
-            elif vs is None and hyperstack_generate.get_files_number(dirpath, tiff_ext) == 1:
-                print("The number of image files is less than 2. For hyperstack it should be at least 2. Skipping")
-                continue
-        if os.path.abspath(update_input_dir) == os.path.abspath(precrop_input_dir):
-            for filename in os.listdir(subdir):
-                os.remove(os.path.join(subdir, filename))
-            shutil.rmtree(os.path.join(subdir))
-
-    print("Run is finished")
-    return
-
-
-if __name__ in ['__builtin__', '__main__']:
-    start_time = time.time()
-    main()
-    end_time = time.time()
-    print("\nDuration of the program execution:")
-    print(pt.convert(end_time - start_time))
-    System.exit(0)
