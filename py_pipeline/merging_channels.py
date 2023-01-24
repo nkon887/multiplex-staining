0d6a64ed2f82269398a63ac7c5fdc3c469086927
nkon887 <66092531+nkon887@users.noreply.github.com>
2023-01-24 05:30:11 +0100

    fix:remove cropping alignment hyperstacks and cropping for alignment as they are redundant. they are integrated main.py merging channels main function as it is used in main.py

--- py_pipeline/merging_channels.py
+++ py_pipeline/merging_channels.py
@@ -1,14 +1,11 @@
 import re
 import sys
 import os
-import time
-from java.lang import System
 from ij.gui import GenericDialog
 from ij import IJ, WindowManager
 from ij.io import FileSaver
 
 sys.path.append(os.path.abspath(os.getcwd()))
-import config
 import pythontools as pt
 
 
@@ -67,25 +64,28 @@ class MergingChannels:
 
     def merging(self, dapi_file, selected_channel_files, output_dir, force_save):
         for channel_file in selected_channel_files:
-            print(str(channel_file))
-            merged_filename = (os.path.basename(channel_file)).split(".")[0] + "_merged_dapi" + self.tiff_ext
-            imp1 = IJ.openImage(dapi_file)
-            imp1.show()
-            imp2 = IJ.openImage(channel_file)
-            imp2.show()
-            IJ.run(imp1, "Merge Channels...",
-                   "c1=" + os.path.basename(channel_file) + " c3=" + os.path.basename(dapi_file) + " keep")
-            imp1.close()
-            imp2.close()
             subfolder_folder_path = os.path.join(output_dir, os.path.basename(channel_file).split("_")[1])
             if not os.path.exists(subfolder_folder_path):
                 os.mkdir(subfolder_folder_path)
+            merged_filename = (os.path.basename(channel_file)).split(".")[0] + "_merged_dapi" + self.tiff_ext
             merged_file_path = os.path.join(subfolder_folder_path, merged_filename)
-            print("Saving the " + str(merged_file_path))
             if (not os.path.exists(merged_file_path)) or force_save:
+                print(str(channel_file))
+                imp1 = IJ.openImage(dapi_file)
+                imp1.show()
+                imp2 = IJ.openImage(channel_file)
+                imp2.show()
+                IJ.run(imp1, "Merge Channels...",
+                       "c1=" + os.path.basename(channel_file) + " c3=" + os.path.basename(dapi_file) + " keep")
+                imp1.close()
+                imp2.close()
                 res = WindowManager.getCurrentImage()
+                print("Saving the " + str(merged_file_path))
                 FileSaver(res).saveAsTiff(merged_file_path)
                 res.close()
+            else:
+                print("Skipping as " + merged_file_path + " exists")
+
         IJ.run("Close All")
 
     def processing(self):
@@ -120,7 +120,7 @@ class MergingChannels:
             for marker in markers:
                 if selected_markers.get(marker) and marker != self.dapi_str + subfolder:
                     selected_channel_files = selected_channel_files + self.get_channel_files(subfolder,
-                                                                                                         marker)
+                                                                                             marker)
             selected_dapi_image = selected_markers[self.dapi_str + subfolder]
             selected_dapi_image_path = os.path.join(subfolder, selected_dapi_image).replace("\\", "/")
             selected_channel_files_paths = []
@@ -128,16 +128,3 @@ class MergingChannels:
                 selected_channel_files_paths.append(
                     os.path.join(subfolder, selected_channel_file).replace("\\", "/"))
             self.merging(selected_dapi_image_path, selected_channel_files_paths, output_dir, force_save)
-
-
-def main():
-    MergingChannels(config.bg_adjust_dir, config.merge_channels_dir, config.tiff_ext, config.dapi_str).processing()
-
-
-if __name__ in ['__builtin__', '__main__']:
-    start_time = time.time()
-    main()
-    end_time = time.time()
-    print("\nDuration of the program execution:")
-    print(pt.convert(end_time - start_time))
-    System.exit(0)
