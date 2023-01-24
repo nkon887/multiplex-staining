c6fa195852319a7befac2942c5fd59cfa002f118
nkon887 <66092531+nkon887@users.noreply.github.com>
2023-01-24 06:15:20 +0100

    feat: image preprocessing package for fiji

---
+++ im-jy-package/src/main/resources/prPackage/pythontools.py
@@ -0,0 +1,44 @@
+import os
+
+
+def find_existing_location(possible_locations, unique_location=1):
+    print("searching " + str(len(possible_locations)) + " locations")
+    location_list = []
+    for location in possible_locations:
+        if os.path.isdir(location):
+            location_list.append(location)
+            print("found location " + location)
+    if len(location_list) == 0:
+        print("no location found")
+    elif unique_location and len(location_list) > 1:
+        print("ambigious locations found:" + str(location_list))
+    return location_list[0]
+
+
+def setting_directory(basedir, dir_name):
+    dir_path = os.path.join(basedir, dir_name).replace("\\", "/")
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+    return dir_path
+
+
+def dapi_tiff_image_filenames(directory, dapi_str, ext):
+    dapi_tiff_files = []
+    files = os.listdir(directory)
+    if not files == []:
+        for filename in sorted(files):
+            if ((dapi_str or dapi_str.upper() or dapi_str.lower()) in filename) and (filename.endswith(ext)):
+                dapi_tiff_files.append(filename)
+    return dapi_tiff_files
+
+
+# to Convert seconds
+# into hours, minutes and seconds
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
