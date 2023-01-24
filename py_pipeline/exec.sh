042aed7b6cc17e63d8985bbb481a088ddf379a08
nkon887 <66092531+nkon887@users.noreply.github.com>
2023-01-24 05:39:51 +0100

    fix:remove cropping alignment hyperstacks and cropping for alignment as they are redundant. they are integrated main.py merging channels remove main function as it is used in main.py created main.py is main execution file hyperstack_generation.py remove main function as it used in main.py change of exec.sh as only two script are needed to be executed

--- py_pipeline/exec.sh
+++ py_pipeline/exec.sh
@@ -4,20 +4,7 @@ echo "MULTIPLEX IMAGE PREPROCESSING"
 #STITCHING
 echo "RUNNING STEP 1 IMAGE PREPARATION"
 python image_preparation.py
-echo  "RUNNING STEP 2 ALIGNMENT"
-$FIJIPATH --ij2 --run alignment.py
-echo  "RUNNING STEP 3 HYPERSTACK GENERATION OF NOT ALIGNED DATA"
-$FIJIPATH --ij2 --run hyperstack_generation.py
-echo  "RUNNING STEP 4 CROPPING TIFF IMAGES"
-$FIJIPATH --ij2 --run cropping_for_alignment.py
-echo  "RUNNING STEP 5 ALIGNMENT PRECROP"
-$FIJIPATH --ij2 --run alignment.py
-echo  "RUNNING STEP 6 CROPPING ALIGNED HYPERSTACKS"
-$FIJIPATH --ij2 --run cropping_aligned_hyperstacks.py
-echo "RUNNING STEP 7 BACKGROUND ADJUSTMENT SUBSTRACTION ALIGNED STACKS"
-$FIJIPATH --ij2 --run background_processing.py
-echo "RUNNING STEP 8 MERGING OF CHANNELS"
-$FIJIPATH --ij2 --run merging_channels.py
+$FIJIPATH --ij2 --run main.py
 #contrast
 #SEGMENTATION
 #binary mask
