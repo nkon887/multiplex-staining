ab1989d3d782bb982dbe762651745b79de608ed7
nkon887 <66092531+nkon887@users.noreply.github.com>
2023-04-21 18:29:09 +0200

    fix: adding function pause to the bash file exe.sh in wdir_scripts

--- wdir_scripts/exec.sh
+++ wdir_scripts/exec.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 clear
+function pause(){
+  read -s -n 1 -p "Press any key to continue . . ."
+  echo ""
+}
+
 echo "MULTIPLEX IMAGE PREPROCESSING"
 SCRIPTPATH=$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
 sub=${SCRIPTPATH:0:2}
@@ -12,6 +17,10 @@ VAR2='step="'"stitch"'"'
 echo "RUNNING STEP 0 STITCHING"
 $FIJIPATH --ij2 --run macro.py $VAR1,$VAR2
 echo "RUNNING STEP 1 IMAGE PREPARATION"
+source /c/Users/nko88/anaconda3/etc/profile.d/conda.sh
+echo "PLEASE ADD CHANNEL MARKERS IN INFOS.TXT"
+## Pause it ##
+pause
 conda activate multiplex
 python image_preparation.py
 VAR2='step="'"alignment"'"'
