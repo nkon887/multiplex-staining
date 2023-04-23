7593e27fadf501312f548a9647b15cc3f2354fbe
nkon887 <66092531+nkon887@users.noreply.github.com>
2023-04-23 17:16:09 +0200

    fix: adjusting preparation_dapi_seg.py, fixing the errors with input subfolders

--- wdir_scripts/preparation_dapi_seg.py
+++ wdir_scripts/preparation_dapi_seg.py
@@ -18,28 +18,28 @@ class PreparationDapiSeg:
 
     def process(self):
         # get all files of directory
-        for filename in os.listdir(self.input_folder):
-            if "0dapi" in str(filename):
-                # adjust contrast of each files (substract 5 from intensity)
-                # subtract 5 from all pixels in our image and make it darker
-                image = cv2.imread(os.path.join(self.input_folder, filename))
-                M = np.ones(image.shape, dtype="uint8") * 5
-                subtracted = cv2.subtract(image, M)
-                file_folder_name = os.path.splitext(os.path.basename(filename))[0]
-                # create folders with files
-                filefolder_path = os.path.join(self.output_folder, file_folder_name)
-                if not os.path.exists(filefolder_path):
-                    os.mkdir(filefolder_path)
-                output_path = os.path.join(filefolder_path, filename)
-                # create folder input
-                gray = cv2.cvtColor(subtracted, cv2.COLOR_BGR2GRAY)
-                sub = Image.fromarray(gray.astype(np.uint8))
-                sub.save(output_path)
-                print('done')
-                f = open(os.path.join(self.output_folder, "channelNames_" + file_folder_name + ".txt"), "w+")
-                f.write(filename)
-                f.close()
-
+        for subdir in [x[0] for x in os.walk(self.input_folder)]:
+            for filename in os.listdir(subdir):
+                if "0dapi" in str(filename):
+                    # adjust contrast of each files (substract 5 from intensity)
+                    # subtract 5 from all pixels in our image and make it darker
+                    image = cv2.imread(os.path.join(self.input_folder, subdir, filename))
+                    M = np.ones(image.shape, dtype="uint8") * 5
+                    subtracted = cv2.subtract(image, M)
+                    file_folder_name = os.path.splitext(os.path.basename(filename))[0]
+                    # create folders with files
+                    filefolder_path = os.path.join(self.output_folder, file_folder_name)
+                    if not os.path.exists(filefolder_path):
+                        os.mkdir(filefolder_path)
+                    output_path = os.path.join(filefolder_path, filename)
+                    # create folder input
+                    gray = cv2.cvtColor(subtracted, cv2.COLOR_BGR2GRAY)
+                    sub = Image.fromarray(gray.astype(np.uint8))
+                    sub.save(output_path)
+                    print('done')
+                    f = open(os.path.join(self.output_folder, "channelNames_" + file_folder_name + ".txt"), "w+")
+                    f.write(filename)
+                    f.close()
 
 def main():
     PreparationDapiSeg(config.bg_adjust_dir, config.dapi_seg_input_dir).process()
