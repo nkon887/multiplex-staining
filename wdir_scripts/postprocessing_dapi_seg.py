1657a0afc02f287e737c6a69508b3c99ee99d977
nkon887 <nkon887@gmail.com>
2023-05-06 16:47:14 +0200

    fix: remove redundant commented text from wdir_scripts/postprocessing_dapi_seg.py

--- wdir_scripts/postprocessing_dapi_seg.py
+++ wdir_scripts/postprocessing_dapi_seg.py
@@ -6,7 +6,7 @@ from PIL import Image
 import cv2
 import numpy as np
 from skimage import exposure
-
+import config
 sys.path.append(os.path.abspath(os.getcwd()))
 
 import pythontools as pt
@@ -23,9 +23,7 @@ class PostProcessingDapiSeg:
         # Load with PIL
         for im in os.listdir(self.input_folder):
             image_file_path = os.path.join(self.input_folder, im)
-            # print(image_file_path)
             if im.endswith(".tif") and not (os.path.isdir(im)):
-                # print(image_file_path)
                 image_file = Image.open(image_file_path)
 
                 # Make into Numpy array and normalise
@@ -44,18 +42,17 @@ class PostProcessingDapiSeg:
                 mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, se2)
 
                 out = na * mask
-                # print(str(out))
                 threshold = 0
                 out[out > threshold] = 1
                 # Save
                 intimg = exposure.rescale_intensity(out, in_range=(0, 1))
                 cv2.imwrite(os.path.join(self.output_folder, im), intimg)
-                print("done")
+        print("Postprocessing is finished")
 
 
 def main():
-    PostProcessingDapiSeg("S:/C13/Microscopy-core/zkobus/230220_Copies_DAPI_for_new_masks/output/visual_output",
-                          "S:/C13/Microscopy-core/zkobus/230220_Copies_DAPI_for_new_masks/output/binary").process()
+    PostProcessingDapiSeg(os.path.join(config.dapi_seg_output_dir, "visual_output"),
+                          os.path.join(config.dapi_seg_binary_dir)).process()
 
 
 if __name__ == "__main__":
