a07a0c655c0ea87372c118d0f4bd0d41d183ac48
nkon887 <66092531+nkon887@users.noreply.github.com>
2023-05-31 16:13:06 +0200

    fix: adding time acquisition, adjusting of the view design, restructuring the code (variables, functions), solving the redundant cmd output by outputCapture

--- wdir_scripts/gui.py
+++ wdir_scripts/gui.py
@@ -3,113 +3,118 @@ import errno
 import os
 import shutil
 import subprocess
-from functools import partial
+import time
 import tkinter as tk
+from functools import partial
 from tkinter import *
 from tkinter import messagebox, filedialog
+
+import pythontools as pt
 
 
 # Defining App to
 # create necessary tkinter widgets
 class App:
     def __init__(self, master):
+        # Creating tkinter variable
+        self.sourceLocation = StringVar()
+        self.destinationLocation = StringVar()
+        self.patterns = StringVar()
+        # Creating the loc variable
+        self.base_dir = os.getcwd()
+        self.left_frame = Frame(master, background="black")
+        self.line = Frame(master, height=400, width=1, bg="grey80", relief='groove')
+        self.right_frame = Frame(master, background="black")
         self.files_dir = ''
-        left_frame = Frame(master, background="black")
-        right_frame = Frame(master, background="black")
-        line = Frame(master, height=400, width=1, bg="grey80", relief='groove')
-        self.button = Button(left_frame,
-                             text="QUIT", fg="red",
-                             command=left_frame.quit, width=30)
-        self.button.pack(side=tk.TOP, pady=10, padx=20)
         self.create_conda_environment("multiplex", "env_multiplex.yml")
-        self.main_input_Label = Label(right_frame, text="INPUT/OUTPUT ", bg="black", fg="white", width=20, height=1)
-        self.main_input_Label.grid(row=0, column=2, pady=5, padx=5, columnspan=2)
-
-        self.link_Label = Label(right_frame, text="Select The Dir To Copy : ", bg="#E8D579", width=20,
-                                height=1)
-        self.link_Label.grid(row=1, column=1, pady=5, padx=5)
-        self.sourceText = Entry(right_frame, width=50, textvariable=sourceLocation)
-        self.sourceText.grid(row=1, column=2, pady=5, padx=5, columnspan=2)
-        self.source_browseButton = Button(right_frame, text="Browse",
-                                          command=self.SourceBrowse, width=15)
-        self.source_browseButton.grid(row=1, column=4, pady=5, padx=5)
-        self.patterns_Label = Label(right_frame, text="Put in patterns of files or folder names to exclude",
-                                    bg="#E8D579", width=20, height=1)
-        self.patterns_Label.grid(row=2, column=1, pady=5, padx=5)
-        self.patterns_Text = Entry(right_frame, width=50, textvariable=patterns)
-        self.patterns_Text.grid(row=2, column=2, pady=5, padx=5, columnspan=2)
-        self.destinationLabel = Label(right_frame, text="Select The Destination : ", bg="#E8D579", width=20,
-                                      height=1)
-        self.destinationLabel.grid(row=3, column=1, pady=5, padx=5)
-        self.destinationText = Entry(right_frame, width=50, textvariable=destinationLocation)
-        self.destinationText.grid(row=3, column=2, pady=5, padx=5, columnspan=2)
-        self.dest_browseButton = Button(right_frame, text="Browse",
-                                        command=self.DestinationBrowse, width=15)
-        self.dest_browseButton.grid(row=3, column=4, pady=5, padx=5)
-        self.copyButton = Button(right_frame, text="Copy File(s)",
-                                 command=self.CopyFile, width=15)
-        self.copyButton.grid(row=4, column=2, pady=5, padx=5)
-        self.moveButton = Button(right_frame, text="Move File(s)",
-                                 command=self.MoveFile, width=15)
-        self.moveButton.grid(row=4, column=3, pady=5, padx=5)
-
-        self.process = Button(left_frame, text="STITCHING".upper(),
+        self.button = Button(self.left_frame,
+                             text="QUIT", fg="red",
+                             command=self.left_frame.quit, width=30)
+        self.button.pack(side=tk.TOP, pady=10, padx=20)
+        self.process = Button(self.left_frame, text="STITCHING".upper(),
                               command=partial(self.run_shell_command, [["fiji", "", "STITCHING"]]), width=30)
         self.process.pack(side=tk.TOP, pady=10, padx=20)
-        self.process = Button(left_frame,
+        self.process = Button(self.left_frame,
                               text="IMAGE PREPARATION".upper(),
                               command=partial(self.run_shell_command, [["python", "multiplex",
                                                                         "image_preparation.py"]]), width=30)
         self.process.pack(side=tk.TOP, pady=10, padx=20)
-        self.process = Button(left_frame,
+        self.process = Button(self.left_frame,
                               text="ALIGNMENT".upper(),
                               command=partial(self.run_shell_command, [["fiji", "", "ALIGNMENT"]]), width=30)
         self.process.pack(side=tk.TOP, pady=10, padx=20)
-        self.process = Button(left_frame,
+        self.process = Button(self.left_frame,
                               text="REALIGNMENT".upper(),
                               command=partial(self.run_shell_command, [['fiji', "", "REALIGNMENT"]]), width=30)
         self.process.pack(side=tk.TOP, pady=10, padx=20)
-        self.process = Button(left_frame,
+        self.process = Button(self.left_frame,
                               text="CROPPING".upper(),
                               command=partial(self.run_shell_command, [["fiji", "", "CROPPING AFTER ALIGNMENT"]]),
                               width=30)
         self.process.pack(side=tk.TOP, pady=10, padx=20)
 
-        self.process = Button(left_frame,
+        self.process = Button(self.left_frame,
                               text="BACKGROUNDADJUSTMENT".upper(),
                               command=partial(self.run_shell_command, [["fiji", "", "BACKGROUNDADJUSTMENT"]]), width=30)
         self.process.pack(side=tk.TOP, pady=10, padx=20)
-        self.process = Button(left_frame,
+        self.process = Button(self.left_frame,
                               text="MERGING CHANNELS".upper(),
                               command=partial(self.run_shell_command, [["fiji", "", "MERGING CHANNELS"]]), width=30)
         self.process.pack(side=tk.TOP, pady=10, padx=20)
         self.create_conda_environment("cellsegsegmenter", "env_cellsegsegmenter.yml")
-        self.process = Button(left_frame,
+        self.process = Button(self.left_frame,
                               text="DapiSeg Segmentation".upper(),
-                              command=partial(self.run_shell_command, [["python", "multiplex", "preparation_dapi_seg"
-                                                                                               ".py"],
-                                                                       ["python", "cellsegsegmenter", "dapi_seg_main"
-                                                                                                      ".py"],
+                              command=partial(self.run_shell_command, [["python", "multiplex", "preparation_dapi_seg.py"
+                                                                        ],
+                                                                       ["python", "cellsegsegmenter", "dapi_seg_main.py"
+                                                                        ],
                                                                        ["python", "multiplex",
-                                                                        "postprocessing_dapi_seg"
-                                                                        ".py"], ["fiji", '', "DAPISEG_RESIZER"]]),
-                              width=30)
+                                                                        "postprocessing_dapi_seg.py"],
+                                                                       ["fiji", "", "DAPISEG_RESIZER"]]), width=30)
         self.process.pack(side=tk.TOP, pady=10, padx=20)
-        left_frame.pack(side=tk.LEFT)
-        line.pack(side=tk.LEFT, padx=10)
-        right_frame.pack(side=tk.LEFT)
+        self.main_input_Label = Label(self.right_frame, text="INPUT/OUTPUT ", bg="black", fg="white", width=20,
+                                      height=1)
+        self.main_input_Label.grid(row=0, column=2, pady=5, padx=5, columnspan=2)
+        self.patterns_Label = Label(self.right_frame, text="Name Pattern Exceptions: ",
+                                    bg="#E8D579", width=20, height=1)
+        self.patterns_Label.grid(row=1, column=1, pady=5, padx=5)
+        self.patterns_Text = Entry(self.right_frame, width=50, textvariable=self.patterns)
+        self.patterns_Text.grid(row=1, column=2, pady=5, padx=5, columnspan=2)
+        self.link_Label = Label(self.right_frame, text="Select The Dir To Copy: ", bg="#E8D579", width=20,
+                                height=1)
+        self.link_Label.grid(row=2, column=1, pady=5, padx=5)
+        self.sourceText = Entry(self.right_frame, width=50, textvariable=self.sourceLocation)
+        self.sourceText.grid(row=2, column=2, pady=5, padx=5, columnspan=2)
+        self.source_browseButton = Button(self.right_frame, text="Browse",
+                                          command=self.SourceBrowse, width=15)
+        self.source_browseButton.grid(row=2, column=4, pady=5, padx=5)
+        self.destinationLabel = Label(self.right_frame, text="Select The Destination: ", bg="#E8D579", width=20,
+                                      height=1)
+        self.destinationLabel.grid(row=3, column=1, pady=5, padx=5)
+        self.destinationText = Entry(self.right_frame, width=50, textvariable=self.destinationLocation)
+        self.destinationText.grid(row=3, column=2, pady=5, padx=5, columnspan=2)
+        self.dest_browseButton = Button(self.right_frame, text="Browse",
+                                        command=self.DestinationBrowse, width=15)
+        self.dest_browseButton.grid(row=3, column=4, pady=5, padx=5)
+        self.copyButton = Button(self.right_frame, text="Copy File(s)",
+                                 command=self.CopyFile, width=15)
+        self.copyButton.grid(row=4, column=2, pady=5, padx=5)
+        self.moveButton = Button(self.right_frame, text="Move File(s)",
+                                 command=self.MoveFile, width=15)
+        self.moveButton.grid(row=4, column=3, pady=5, padx=5)
+        self.left_frame.pack(side=tk.LEFT)
+        self.line.pack(side=tk.LEFT, padx=10)
+        self.right_frame.pack(side=tk.LEFT)
 
     def run_shell_command(self, parametersets):
         for parameterset in parametersets:
             package, env, command = parameterset
-            if package == "python" and env != '':
-                subprocess.run('conda activate ' + env + ' && ' + package + ' ' + command + ' && conda deactivate',
-                               shell=True, check=True)
+            if package == "python" and env != "":
+                subprocess.Popen(f"conda activate {env} && {package} {command} && conda deactivate", shell=True)
             elif package == "fiji":
                 subprocess.Popen(
-                    "%FIJIPATH% --ij2 --run macro.py \"base_dir='" + base_dir + "' , step = '" + command + "'\"",
-                    shell=True,
-                    stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    f"%FIJIPATH% --ij2 --run macro.py \"base_dir='{self.base_dir}' , step = '{command}'\"",
+                    shell=True, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
             else:
                 "Not correct shell command. Please check it"
@@ -117,13 +122,12 @@ class App:
     def create_conda_environment(self, env_name, requirements_file):
         env_exists = False
         try:
-            subprocess.run(f"conda activate {env_name}", shell=True, check=True)
+            subprocess.run(f"conda activate {env_name}", shell=True, capture_output=True)
             env_exists = True
         except subprocess.CalledProcessError as e:
             pass
-
         if not env_exists:
-            subprocess.run(f"conda env create -f {requirements_file}", shell=True)
+            subprocess.Popen(f"conda env create -f {requirements_file}", shell=True)
             print(f"Conda environment {env_name} created.")
         else:
             print(f"Conda environment {env_name} already exists.")
@@ -138,7 +142,7 @@ class App:
         # self.files_list = list(
         #    filedialog.askopenfilenames(initialdir=os.path.join(base_dir, "workingDir")))
 
-        self.files_dir = filedialog.askdirectory(initialdir=os.path.join(base_dir, "workingDir"))
+        self.files_dir = filedialog.askdirectory(initialdir=os.path.join(self.base_dir, "workingDir"))
 
         # Displaying the selected files in the root.sourceText
         self.sourceText.delete(0, tk.END)  # Remove current text in entry
@@ -152,7 +156,7 @@ class App:
         # filedialog.askopendirectory() method
         # Setting initialdir argument is optional
         destinationdirectory = filedialog.askdirectory(
-            initialdir=os.path.join(base_dir, "workingDir", "00_raw_input"))
+            initialdir=os.path.join(self.base_dir, "workingDir", "00_raw_input"))
         # Displaying the selected files in the root.sourceText
         self.destinationText.delete(0, tk.END)  # Remove current text in entry
         # Displaying the selected directory in the
@@ -163,18 +167,15 @@ class App:
         # user in the SourceBrowse() and storing it in a
         # variable named files_dir
         files_dir = self.files_dir
-        patterns_list = patterns.get().split()
+        patterns_list = self.patterns.get().split()
         # Retrieving the destination location from the
         # textvariable using destinationLocation.get() and
         # storing in destination_location
-        destination_location = destinationLocation.get()
-        # Looping through the files present in the list
-        # for f in files_dir:
+        destination_location = self.destinationLocation.get()
         # Copying the file to the destination using
         # the copy() of shutil module copy take the
         # source file and the destination folder as
         # the arguments
-        # shutil.copy(f, destination_location)
         try:
             # if path already exists, remove it before copying with copytree()
             if os.path.exists(destination_location):
@@ -196,22 +197,18 @@ class App:
         # Retrieving the source file selected by the
         # user in the SourceBrowse() and storing it in a
         # variable named files_list
-        # files_list = self.files_dir
         files_dir = self.files_dir
-        patterns_list = patterns.get().split()
+        patterns_list = self.patterns.get().split()
         # Retrieving the destination location from the
         # textvariable using destinationLocation.get() and
         # storing in destination_location
-        destination_location = destinationLocation.get()
+        destination_location = self.destinationLocation.get()
 
         # Looping through the files present in the list
-        # for f in files_list:
         # Moving the file to the destination using
         # the move() of shutil module copy take the
         # source file and the destination folder as
         # the arguments
-        #    shutil.move(f, destination_location)
-
         for file in os.listdir(files_dir):
             if file not in patterns_list:
                 try:
@@ -233,16 +230,19 @@ class App:
         self.destinationText.delete(0, tk.END)
 
 
-window = Tk()
-# Creating tkinter variable
-sourceLocation = StringVar()
-destinationLocation = StringVar()
-patterns = StringVar()
-# Creating global variable
-base_dir = os.getcwd()
-# Calling the App class function
-app = App(window)
-window.title("Running the Steps of Multiplex Pipeline")
-window.geometry('880x450')
-window.config(background="black")
-window.mainloop()
+def main():
+    window = Tk()
+    # Calling the App class function
+    App(window)
+    window.title("Running the Steps of Multiplex Pipeline")
+    window.geometry('880x450')
+    window.config(background="black")
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    end_time = time.time()
+    print("\nDuration of the program execution:")
+    print(pt.convert(end_time - start_time))
