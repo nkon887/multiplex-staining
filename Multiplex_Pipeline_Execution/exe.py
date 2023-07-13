039b2e5b4451895c95b4f0b859d3c71eec6a1249
nkon887 <66092531+nkon887@users.noreply.github.com>
2023-07-13 14:10:50 +0200

    fix: adjust python version for myenv in exe.py

--- Multiplex_Pipeline_Execution/exe.py
+++ Multiplex_Pipeline_Execution/exe.py
@@ -11,7 +11,7 @@ def create_conda_environment(env_name):
         pass
     if not env_exists:
         subprocess.run(
-            f"conda create -y --name {env_name} python=3.10 pip && conda activate {env_name} && pip install gdown && "
+            f"conda create -y --name {env_name} python=3.6 pip && conda activate {env_name} && pip install gdown && "
             f"pip install pandas && pip install tensorflow==1.13.1 && "
             f"pip install \"git+https://github.com/nkon887/multiplex-staining.git#multiplex&subdirectory"
             f"=Multiplex_package\"",
