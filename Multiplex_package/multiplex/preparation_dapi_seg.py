464a88ffa5cca812899f03f7c490b20a9159e9be
nkon887 <66092531+nkon887@users.noreply.github.com>
2023-07-24 16:28:46 +0200

    fix: adjust preparation_dapi_seg.py of the package multiplex to segment only dapi file of the second date and with background substraction

--- Multiplex_package/multiplex/preparation_dapi_seg.py
+++ Multiplex_package/multiplex/preparation_dapi_seg.py
@@ -6,6 +6,7 @@ from PIL import Image
 import multiplex.setup_logger
 import logging
 import multiplex.helpertools as ht
+
 # multiplex.preparation_dapi_seg.py creates its own logger, as a sub logger to 'multiplex.main'
 logger = logging.getLogger('multiplex.main.preparation_dapiSeg')
 
@@ -17,26 +18,58 @@ class PreparationDapiSeg:
         self.dapi_str = dapi_str
 
     def process(self):
-        # get all files of directory
-        for subdir in [x[0] for x in os.walk(self.input_folder)]:
+        dapis_selected = {}
+        for subdir in [x[0] for x in os.walk(self.input_folder) if not x[0] in self.input_folder]:
+            dapis = []
             for filename in os.listdir(subdir):
-                if f"0{self.dapi_str}" in str(filename):
-                    # adjust contrast of each files (substract 5 from intensity)
-                    # subtract 5 from all pixels in our image and make it darker
-                    image = cv2.imread(ht.correct_path(self.input_folder, subdir, filename))
-                    m = np.ones(image.shape, dtype="uint8") * 5
-                    subtracted = cv2.subtract(image, m)
-                    file_folder_name = os.path.splitext(os.path.basename(filename))[0]
-                    # create folders with files
-                    filefolder_path = ht.correct_path(self.output_folder, file_folder_name)
-                    if not os.path.exists(filefolder_path):
-                        os.mkdir(filefolder_path)
-                    output_path = ht.correct_path(filefolder_path, filename)
-                    # create folder input
-                    gray = cv2.cvtColor(subtracted, cv2.COLOR_BGR2GRAY)
-                    sub = Image.fromarray(gray.astype(np.uint8))
-                    sub.save(output_path)
-                    f = open(ht.correct_path(self.output_folder, "channelNames_" + file_folder_name + ".txt"), "w+")
-                    f.write(filename)
-                    f.close()
+                if f"0{self.dapi_str}" in str(filename) and "_backgroundSub" in str(filename):
+                    dapis.append(filename)
+                dapis.sort()
+            dapis_selected[subdir] = dapis[1]
+        # get all files of directory
+        for key in dapis_selected:
+            # adjust contrast of each file (substract 5 from intensity)
+            # subtract 5 from all pixels in our image and make it darker
+            subdir = key
+            filename = dapis_selected[key]
+            image = cv2.imread(ht.correct_path(self.input_folder, subdir, filename))
+            m = np.ones(image.shape, dtype="uint8") * 5
+            subtracted = cv2.subtract(image, m)
+            file_folder_name = os.path.splitext(os.path.basename(filename))[0]
+            # create folders with files
+            filefolder_path = ht.correct_path(self.output_folder, file_folder_name)
+            if not os.path.exists(filefolder_path):
+                os.mkdir(filefolder_path)
+            output_path = ht.correct_path(filefolder_path, filename)
+            # create folder input
+            gray = cv2.cvtColor(subtracted, cv2.COLOR_BGR2GRAY)
+            sub = Image.fromarray(gray.astype(np.uint8))
+            sub.save(output_path)
+            f = open(ht.correct_path(self.output_folder, "channelNames_" + file_folder_name + ".txt"), "w+")
+            f.write(filename)
+            f.close()
         logger.info('Preparation of input for dapi segmentation is finished')
+
+        # # get all files of directory
+        # for subdir in [x[0] for x in os.walk(self.input_folder)]:
+        #     for filename in os.listdir(subdir):
+        #         if f"0{self.dapi_str}" in str(filename):
+        #             # adjust contrast of each file (substract 5 from intensity)
+        #             # subtract 5 from all pixels in our image and make it darker
+        #             image = cv2.imread(ht.correct_path(self.input_folder, subdir, filename))
+        #             m = np.ones(image.shape, dtype="uint8") * 5
+        #             subtracted = cv2.subtract(image, m)
+        #             file_folder_name = os.path.splitext(os.path.basename(filename))[0]
+        #             # create folders with files
+        #             filefolder_path = ht.correct_path(self.output_folder, file_folder_name)
+        #             if not os.path.exists(filefolder_path):
+        #                 os.mkdir(filefolder_path)
+        #             output_path = ht.correct_path(filefolder_path, filename)
+        #             # create folder input
+        #             gray = cv2.cvtColor(subtracted, cv2.COLOR_BGR2GRAY)
+        #             sub = Image.fromarray(gray.astype(np.uint8))
+        #             sub.save(output_path)
+        #             f = open(ht.correct_path(self.output_folder, "channelNames_" + file_folder_name + ".txt"), "w+")
+        #             f.write(filename)
+        #             f.close()
+        # logger.info('Preparation of input for dapi segmentation is finished')
